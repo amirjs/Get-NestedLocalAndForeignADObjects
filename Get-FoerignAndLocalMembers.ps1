33d0cef58b332082f265bbc0bdf22886f1afc43f
Amir Joseph Sayes <amirjsayes@gmail.com>
2022-11-18 14:51:21 +0000

    Improvments

--- Get-FoerignAndLocalMembers.ps1
+++ Get-FoerignAndLocalMembers.ps1
@@ -1,17 +1,21 @@
 Function Get-ForeignAndLocalMembers {
    <#
     .SYNOPSIS       
-
+        Recursively get all members of given group(s) and retrieve their nested groups and users including foreign principles from other domains/forests and show their parent group.
     .DESCRIPTION        
 
-    .PARAMETER         
+    .PARAMETER Groups
+    The AD group(s) names of AD groups that the function will get members of.  
 
-    .PARAMETER         
+    .PARAMETER Domains
+    The FQDNs of all possible domains in your environemnt.    
 
+    .PARAMETER Server
+    The local Domain name of the group(s) being queried initially by the function. By default, the function will get the current machine domain.
     .EXAMPLE
         
     .INPUTS
-        Inputs (if any)
+        You can pipe the group mame(s) into the command which is recognised by type, you can also pipe any parameter by name. 
     .OUTPUTS
         Output (if any)
     .NOTES
@@ -19,7 +23,7 @@ Function Get-ForeignAndLocalMembers {
         VERSION:     1.0.4
         DateCreated: 2020-04-15
         DateUpdated: 2019-07-28
-    #>
+    #>    
     [CmdletBinding(DefaultParameterSetName = 'Groups')]    
     param (
         [Parameter(ValueFromPipelineByPropertyName, Mandatory = $True)]
@@ -33,16 +37,21 @@ Function Get-ForeignAndLocalMembers {
     )    
         Begin {
             #Check if the current session is running in full language mode, otherwise, exit.
-
-
             #Build a hash table of NetBios and Domain names based on $Domains variable entered by the user
             $DomainsTable = @()
             Foreach ($DomainEntry in $Domains) {                
-                $BuildHash = Get-ADDomain -Identity $DomainEntry                 
-                $DomainsTable +=  [PSCustomObject]@{
-                    "DomainName" = $($BuildHash.name)
-                    "NetBIOSName" = $($BuildHash.NetBIOSName)
-                    "DNSRoot" = $($BuildHash.DNSRoot)
+                Try {
+                    $BuildHash = Get-ADDomain -Identity $DomainEntry -Server $DomainEntry -ErrorAction Stop   
+                    $DomainsTable +=  [PSCustomObject]@{
+                        "DomainName" = $($BuildHash.name)
+                        "NetBIOSName" = $($BuildHash.NetBIOSName)
+                        "DNSRoot" = $($BuildHash.DNSRoot)
+                    }
+                }
+                Catch {
+                    $err = $error[0]
+                    Write-Error "Domain $($DomainEntry) could not be found. Please ensure the domain name is correct and you have rights to connect/read the domain. Full error: $err"                    
+                    exit
                 }
             }            
         }
@@ -54,17 +63,17 @@ Function Get-ForeignAndLocalMembers {
                 #Get the "members" property of the groups and pipe it into Get-ADObject 
                 $NestedGroupsAndUsers = $GroupProperties.Members | Get-ADObject -Properties * -Server $Server -ErrorAction SilentlyContinue                                                
                 #Loop inside the members (users and groups) and query them in their original domain
-                Foreach ($row in $NestedGroupsAndUsers) {                                     
+                Foreach ($row in $NestedGroupsAndUsers) {                                                                             
                     #Use Regex to figure out if a group or a user is a foreign principal i.e. resides in different domain/forest                    
-                    if($row.name -match "^S-\d-\d-\d\d") {           
+                    if($row.name -match "^S-\d-\d-\d\d") {                               
                         try {
-                            #Translate the SID of the user/Group to their actual NTAccount name e.g. DomainA\group_name_1, DomainB\User1 
-                            $ObjectNameFromSID =  ([System.Security.Principal.SecurityIdentifier] $row.name).Translate([System.Security.Principal.NTAccount])                                               
+                            #Translate the SID of the user/Group to their actual NTAccount name e.g. DomainA\group_name_1, DomainB\User1                             
+                            $ObjectNameFromSID =  ([System.Security.Principal.SecurityIdentifier]$row.name).Translate([System.Security.Principal.NTAccount])                                                                                                                                   
                         }
                         Catch {                            
                             #If ObjectNameFromSID is not found, it's likely that object does not exist anymore although it is still referenced as a foregin member of the currect AD group
                             #Build an object to highlight those orphened objects                            
-                            If ($null -eq $ObjectNameFromSID) {
+                            If ($null -eq $ObjectNameFromSID) {                                
                                 $NewPSObj = New-Object -TypeName PSobject                                                                
                                 $NewPSObj | Add-Member -MemberType NoteProperty -Name "ParentGroup" -Value "$($GroupEntry)" -Force
                                 $NewPSObj | Add-Member -MemberType NoteProperty -Name "Status" -Value "Couldn't find in target domain" -Force
@@ -72,41 +81,47 @@ Function Get-ForeignAndLocalMembers {
                                 $NewPSObj | Add-Member -MemberType NoteProperty -Name "DisplayName" -Value "$($row.DisplayName)" -Force                                
                                 $AllMemberObjects += $NewPSObj
                                 $NewPSObj = $null
-                                #skip just this iteration, but continue loop
-                                Continue 
+                                #Clean up variables 
+                                $LDAPQuery = $CurrentUserDomainFullName = $ObjectNameFromSID = $null                                                        
+                                #skip just this iteration, but continue loop    
+                                Continue                             
                             }
                         }
                         Try {
                             ##Find the DNSRoot name (Domain full name) based on the current user/group being processed e.g. domain\user
-                            If ($ObjectNameFromSID) {
-                                $CurrentUserDomainFullName = $DomainsHashTable.where({$_.NetBIOSName -like ($ObjectNameFromSID.value.split("\")[0])}).DNSRoot                                                     
+                            If ($ObjectNameFromSID) {                                
+                                $CurrentUserDomainFullName = $DomainsTable.where({$_.NetBIOSName -like ($ObjectNameFromSID.value.split("\")[0])}).DNSRoot                                                                                                                                                     
                             }                            
                         }
                         Catch {
                             Write-Error "Could not find a matching domain for $($ObjectNameFromSID) from the list of provided domains: $($DomainsHashTable.NetBIOSName) .Full error: $($Error[0])"
-                            #skip just this iteration, but continue loop
-                            Continue
+                            #Clean up variables 
+                            $LDAPQuery = $CurrentUserDomainFullName = $ObjectNameFromSID = $null                                                        
+                            #skip just this iteration, but continue loop                            
+                            Continue NestedRows                            
                         }                            
                         Try {
-                            #Call the domain found above and retrieve the users data                             
-                            $LDAPQuery = [ADSI]"LDAP://$($CurrentUserDomainFullName)/<SID=$($row.name)>"      
+                            #Call the domain found above and retrieve the users data                                                         
+                            $LDAPQuery = [ADSI]"LDAP://$($CurrentUserDomainFullName)/<SID=$($row.name)>"                                                              
                         }
-                        Catch {
+                        Catch {                            
                             Write-Error 'Error querying AD via LDAP: [ADSI]"LDAP://$($CurrentUserDomainFullName)/<SID=$($row.name)>"'+ "Full error: $($error[0])"
+                            #Clean up variables 
+                            $LDAPQuery = $CurrentUserDomainFullName = $ObjectNameFromSID = $null                                                        
                             #skip just this iteration, but continue loop
-                            continue
+                            Continue NestedRows                            
                         }   
-                        #If the current retrived object is a user, grab their information 
+                        #If the current retrieved object is a user, grab their information 
                         if ($LDAPQuery.SchemaClassName -eq "user") {                                
                             $NewPSObj = New-Object -TypeName PSobject                                
                             $NewPSObj = Get-ADObject -Identity $($LDAPQuery.distinguishedName) -Server $CurrentUserDomainFullName -Properties *
                             $NewPSObj | Add-Member -MemberType NoteProperty -Name "ParentGroup" -Value "$($GroupEntry)" -Force
+                            $NewPSObj | Add-Member -MemberType NoteProperty -Name "ObjectDomain" -Value "$($CurrentUserDomainFullName)" -Force
                             $AllMemberObjects += $NewPSObj
                             $NewPSObj = $null
                                                         
                             #Clean up variables 
-                            $LDAPQuery = $CurrentUserDomainFullName = $ObjectNameFromSID = $null
-                            $Orphan = $false                                
+                            $LDAPQuery = $CurrentUserDomainFullName = $ObjectNameFromSID = $null                                                        
                         }
                         #If the current retrieved object is a group, recursivly call the function again for that group
                         elseif ($LDAPQuery.SchemaClassName -eq "group") {                                
@@ -114,14 +129,17 @@ Function Get-ForeignAndLocalMembers {
                         }                            
                     }                                  
                     else {                        
-                        #If the object (group or user) is local to the current domain, query it using get-adobject 
+                        #If the object (group or user) is local to the current domain, query it using get-adobject                         
                         $NewPSObj = New-Object -TypeName PSobject
                         $NewPSObj = Get-ADObject -Identity $row.distinguishedName -Server $Server -Properties * 
-                        #Add member property to display the parent group of the current object                                 
+                        #Add member property to display the parent group and domain name of the current object                                 
                         $NewPSObj | Add-Member -MemberType NoteProperty -Name "ParentGroup" -Value "$($GroupEntry)" -Force
+                        $NewPSObj | Add-Member -MemberType NoteProperty -Name "ObjectDomain" -Value "$($Server)" -Force
                         #pipe the new PS object containing the AD object and the added ParentGroup property into the results array 
                         $AllMemberObjects += $NewPSObj
-                        $NewPSObj = $null                        
+                        $NewPSObj = $null     
+                        #Clean up variables 
+                        $LDAPQuery = $CurrentUserDomainFullName = $ObjectNameFromSID = $null                                                                           
                     }                       
                 }               
             #Return all member objects
@@ -132,10 +150,3 @@ Function Get-ForeignAndLocalMembers {
     
 }
 
-# Main 
-
-$Domains = @("uk.corp.investec.com","BWD-RENSBURG.co.uk","investec.corp")
-$result = @()
-$result = Get-ForeignAndLocalMembers -Groups "AmirTest" -Server "uk.corp.investec.com" -Domains $Domains
-$result | select ParentGroup, status, CN, name, DisplayName | ft
-$result.Count
